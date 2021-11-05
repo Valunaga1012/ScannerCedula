37c41c832d51ec53acf57dc686e037f2f204f566
Cesar Romero <cromero@pharoshouse.com>
2021-11-05 17:59:36 -0500

    first commit

---
+++ src/app/constant/constant.ts
@@ -0,0 +1,4 @@
+export const HAS_UPPERCASE = /[A-Z]/g; // Has letter uper case
+export const HAS_LOWECASE = /[a-z]/g; // Has letter lower case
+export const HAS_NUMBER = /[0-9]/g; // Has number
+export const HAS_LETTER_AND_NUMBER = /[^A-Za-z0-9+]+/g;
---
+++ src/app/constant/info.interface.ts
@@ -0,0 +1,8 @@
+export type Cedula = Readonly <{
+    num: number,
+    nombre: string,
+    apellido: string,
+    genero: string,
+    cumple: string,
+    sangre: string,
+}>
