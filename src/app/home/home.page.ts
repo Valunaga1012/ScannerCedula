eb486f5727b336c2c886f70dd9ba1c1b39ffaf7f
Cesar Romero <cromero@pharoshouse.com>
2021-11-08 10:34:54 -0500

    limpiando codigo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,60 +1,56 @@
 import { Component, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-import { BarcodeScanner, SupportedFormat,BarcodeScannerPlugin } from '@capacitor-community/barcode-scanner';
+import { BarcodeScanner, SupportedFormat } from '@capacitor-community/barcode-scanner';
 import { AlertController } from '@ionic/angular';
-import { HAS_LETTER_AND_NUMBER, HAS_UPPERCASE, HAS_NUMBER} from 'src/app/constant/constant'
+import { HAS_LETTER_AND_NUMBER, HAS_UPPERCASE, HAS_NUMBER } from 'src/app/constant/constant'
 import { Cedula } from '../constant/info.interface';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage implements AfterViewInit,OnDestroy,OnInit  {
+export class HomePage implements AfterViewInit, OnDestroy {
   result = null;
   scanActivate = false;
   public info: Cedula;
-    public numIdentificacion: number;
-    public nombre: string;
-    public segNombre: string;
-    public apellido: string;
-    public segApellido: string;
-    public genero: string;
-    public fechaNacimiento: any;
-    public tipoSangre: string;
-    p
-  constructor( private alertController: AlertController) {}
- ngOnInit(){
- }
-  
+  public numIdentificacion: number;
+  public nombre: string;
+  public segNombre: string;
+  public apellido: string;
+  public segApellido: string;
+  public genero: string;
+  public fechaNacimiento: any;
+  public tipoSangre: string;
 
-  ngAfterViewInit(){
+  constructor(private alertController: AlertController) { }
+
+  ngAfterViewInit() {
     BarcodeScanner.prepare()
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     BarcodeScanner.stopScan();
   }
 
   async startScanner() {
-  const allowed = await this.permisoCheck();
-  if(allowed){
-    this.scanActivate = true;
-    const result = await BarcodeScanner.startScan({ targetedFormats: [SupportedFormat.PDF_417] });
-    this.extractColDocumentData(result.content);    
-    if(result.hasContent){
-      this.result = result.content
-      this.scanActivate = false;
+    const allowed = await this.permisoCheck();
+    if (allowed) {
+      this.scanActivate = true;
+      const result = await BarcodeScanner.startScan({ targetedFormats: [SupportedFormat.PDF_417] });
+      this.extractColDocumentData(result.content);
+      if (result.hasContent) {
+        this.result = result.content
+        this.scanActivate = false;
+      }
     }
-  }
-  
-}
 
-  async permisoCheck(){
+  }
+
+  async permisoCheck() {
     return new Promise(async (resolve, reject) => {
-      const status =await BarcodeScanner.checkPermission({force: true});
-      if(status.granted){
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
         resolve(true)
-      }else if(status.denied){
+      } else if (status.denied) {
         const alert = await this.alertController.create({
           header: 'No Permission',
           message: 'please allow camera access in your setting',
@@ -64,21 +60,20 @@ export class HomePage implements AfterViewInit,OnDestroy,OnInit  {
           },
           {
             text: 'Open Setting',
-            handler: () =>{
+            handler: () => {
               BarcodeScanner.openAppSettings();
               resolve(false);
             }
           }]
         });
         await alert.present();
-      }else{
+      } else {
         resolve(false)
       }
-     
     });
   }
 
-  stopScanner(){
+  stopScanner() {
     BarcodeScanner.stopScan();
     this.scanActivate = false;
   }
@@ -86,36 +81,35 @@ export class HomePage implements AfterViewInit,OnDestroy,OnInit  {
   extractColDocumentData(data) {
     const info = data.replace(HAS_LETTER_AND_NUMBER, ' ').split(' ');
     let indexMod = 0;
-  
     if (HAS_UPPERCASE.test(info[3])) {
       indexMod = - 1;
       const idString = info[3].replace(HAS_UPPERCASE, '');
-      this.numIdentificacion = idString.substring(10, idString.length);
+      this.numIdentificacion = idString.substr(10, idString.length);
       this.apellido = info[3].replace(HAS_NUMBER, '');
     } else {
       this.numIdentificacion = info[4].replace(HAS_UPPERCASE, '');
       this.apellido = info[4].replace(HAS_NUMBER, '');
     }
-  
+
     this.segApellido = info[5 + indexMod];
     this.nombre = info[6 + indexMod];
-  
+
     if (!(HAS_NUMBER.test(info[7 + indexMod]))) {
       this.segNombre = info[7 + indexMod];
     }
-        
+
     const extraInfo = info[this.segNombre ? 8 + indexMod : 7 + indexMod];
     this.genero = extraInfo.includes('M') ? 'M' : 'F';
     this.fechaNacimiento = extraInfo.substr(2, 8);
     this.tipoSangre = extraInfo.substr(-2);
-    
-    this.info= {
-     num: this.numIdentificacion,
-     nombre:`${this.nombre} ${this.segNombre || ''}`,
-     apellido: `${this.apellido} ${this.segApellido || ''}`,
-     genero: this.genero,
-     cumple: this.fechaNacimiento.replace(/[^\dA-Z]/g, ' ').replace(/(.{4})/g, '$1/').replace(/(.{7})/g, '$1/').trim(),
-     sangre: this.tipoSangre,
+
+    this.info = {
+      num: this.numIdentificacion,
+      nombre: `${this.nombre} ${this.segNombre || ''}`,
+      apellido: `${this.apellido} ${this.segApellido || ''}`,
+      genero: this.genero,
+      cumple: this.fechaNacimiento.replace(/[^\dA-Z]/g, ' ').replace(/(.{4})/g, '$1/').replace(/(.{7})/g, '$1/').trim(),
+      sangre: this.tipoSangre,
     };
   }
 }
